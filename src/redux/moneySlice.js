4b2158cd7936102f008f0b5cea97e571c196b10c
aysemadrann <aysemadranesm@gmail.com>
2021-09-27 16:07:09 +0300

    edited moneySlice

--- src/redux/moneySlice.js
+++ src/redux/moneySlice.js
@@ -1,15 +1,102 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 export const moneySlice = createSlice({
     name:'moneys',
     initialState:{
-        money: 1000000,
+        items: [
+            {
+                id:nanoid(),
+                img:'https://neal.fun/spend/images/big-mac.jpg',
+                title:'Big Mac',
+                price:'$2',
+                quantity:0,
+            },
+            {
+                id:nanoid(),
+                img:'https://neal.fun/spend/images/flip-flops.jpg',
+                title:'Flip Flops',
+                price:'$3',
+                quantity:0,
+            },
+            {
+                id:nanoid(),
+                img:'https://neal.fun/spend/images/coca-cola-pack.jpg',
+                title:'Coca-cola Pack',
+                price:'$5',
+                quantity:0,
+            },
+            {
+                id:nanoid(),
+                img:'https://neal.fun/spend/images/movie-ticket.jpg',
+                title:'Movie Ticket',
+                price:'$12',
+                quantity:0,
+            },
+            {
+                id:nanoid(),
+                img:'https://neal.fun/spend/images/book.jpg',
+                title:'Book',
+                price:'$15',
+                quantity:0,
+            },
+            {
+                id:nanoid(),
+                img:'https://neal.fun/spend/images/lobster-dinner.jpg',
+                title:'Lobster Dinner',
+                price:'$45',
+                quantity:0,
+            },
+            {
+                id:nanoid(),
+                img:'https://neal.fun/spend/images/video-game.jpg',
+                title:'Video Game',
+                price:'$60',
+                quantity:0,
+            },
+            {
+                id:nanoid(),
+                img:'https://neal.fun/spend/images/amazon-echo.jpg',
+                title:'Amazon Echo',
+                price:'$99',
+                quantity:0,
+            },
+            {
+                id:nanoid(),
+                img:'https://neal.fun/spend/images/year-of-netflix.jpg',
+                title:'Netflix',
+                price:'$100',
+                quantity:0,
+            },
+            {
+                id:nanoid(),
+                img:'https://neal.fun/spend/images/air-jordans.jpg',
+                title:'Air Jordans',
+                price:'$125',
+                quantity:0,
+            },
+            {
+                id:nanoid(),
+                img:'https://neal.fun/spend/images/airpods.jpg',
+                title:'Airpods',
+                price:'$199',
+                quantity:0,
+            },
+            {
+                id:nanoid(),
+                img:'https://neal.fun/spend/images/gaming-console.jpg',
+                title:'Gaming Console',
+                price:'$299',
+                quantity:0,
+                 
+            },
+        ],
+        money:100000,
 
     },
     reducers:{},
     extraReducers:{},
 });
 
-
+export const ProductSelector = (state) => state.moneys.items;
 export const moneySelector = (state) => state.moneys.money;
 export default moneySlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moneySlice from "./moneySlice";
-import productSlice from "./productSlice";
+
 
 
 export const store = configureStore({
     reducer: {
         moneys: moneySlice,
-        products:productSlice,
+
     },
 });
